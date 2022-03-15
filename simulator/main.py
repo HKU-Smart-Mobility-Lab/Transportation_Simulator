d4ea72d8ac6854d31775a0d371a41fd4a7de1614
yuhao101 <yuhao101@connect.hku.hk>
2022-03-15 22:48:06 +0800

    set multi tasks

--- simulator/main.py
+++ simulator/main.py
@@ -11,22 +11,22 @@ warnings.filterwarnings("ignore")
 
 if __name__ == "__main__":
     simulator = Simulator(**env_params)
-    test_num = 5
     driver_num = [500,1000,1500,2000,2500]
+    max_distance_num = [0.5,1,2,3]
     track_record = []
     # track的格式为[{'driver_1' : [[lng, lat, status, time_a], [lng, lat, status, time_b]],
     # 'driver_2' : [[lng, lat, status, time_a], [lng, lat, status, time_b]]},
     # {'driver_1' : [[lng, lat, status, time_a], [lng, lat, status, time_b]]}]
-    for num in range(test_num):
-        simulator.reset()
-        for step in tqdm(range(simulator.finish_run_step)):
-            env_params['driver_num'] = driver_num[num]
-            new_tracks = simulator.step()
-            track_record.append(new_tracks)
-
-    # pickle.dump(track_record, open(data_path + 'toy_records_no_price_500' + '.pickle', 'wb'))
-
-        pickle.dump(track_record, open('./output/records_driver_num_'+str(driver_num[num]) + '.pickle', 'wb'))
+    for single_driver_num in driver_num:
+        for single_max_distance_num in max_distance_num:
+            simulator.reset()
+            for step in tqdm(range(simulator.finish_run_step)):
+                env_params['driver_num'] = single_driver_num
+                env_params['maximal_pickup_distance'] = single_max_distance_num
+                new_tracks = simulator.step()
+                track_record.append(new_tracks)
+            pickle.dump(track_record, open('./output/records_driver_num_'+str(single_driver_num)+'_distance_' +
+                                           str(single_max_distance_num) + '.pickle', 'wb'))
 
 
 
