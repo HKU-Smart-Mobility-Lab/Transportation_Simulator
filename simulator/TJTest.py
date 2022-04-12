b26bb42c361fa0ddd5e0a581b49f29d47b5d0b1e
chenhaoling <813947820@qq.com>
2022-04-12 10:32:30 +0800

    --ignore

--- simulator/TJTest.py
+++ simulator/TJTest.py
@@ -8,6 +8,7 @@ import sys
 import pickle
 import pandas as pd
 from config import env_params
+from tqdm import tqdm
 order = pickle.load(open("input/order.pickle",'rb'))
 # column_name = ['order_id', 'origin_id', 'origin_lat', 'origin_lng', 'dest_id', 'dest_lat', 'dest_lng',
 #                            'trip_distance', 'start_time', 'origin_grid_id', 'dest_grid_id', 'itinerary_node_list',
@@ -26,29 +27,51 @@ order = pickle.load(open("input/order.pickle",'rb'))
 path = "./output"
 files = os.listdir(path)
 files.sort()
-files.pop(0)
 print(files)
 start_time = env_params['t_initial']
 end_time = env_params['t_end']
-for file in files:
+driver_list = []
+pickup_list = []
+delivery_list = []
+cruise_list = []
+matching_rate_list = []
+waiting_time_list = []
+pickup_time_list = []
+pickup_ratio_list = []
+usage_ratio_list = []
+result_pd = pd.DataFrame()
+# result_pd = pd.read_csv("analysis.csv")
+for file in tqdm(files):
     if os.path.isdir(path + "/" + file):
         print("dir",file)
         sub_files = os.listdir(path + "/" + file)
+        sub_files.remove("time_statistic.txt")
+        sub_files.sort(key=lambda x:int(x.split(".")[0].split("_")[-1]))
         for sub_file in sub_files:
             print("sub_file",sub_file)
             if ".pickle" in sub_file and "records" in sub_file:
                 records = pickle.load(open(path + "/" + file + "/" + sub_file,'rb'))
-                print(records[-1])
-                result = []
                 driver_num = int(sub_file.split(".")[0].split("_")[-1])
-                print(driver_num)
-                result.append(get_matching_rate(records,order))
-                result.append(get_avg_prematching_waiting_time(records,order))
-                result.append(get_postmatching_pickup_time(records))
-                result.append(get_driver_pickup_ratio(records,start_time,end_time,driver_num))
-                result.append(get_driver_usage_rate(records,start_time,end_time,driver_num))
-                print(result)
-                sys.exit()
+                pickup_list.append(file.split("_")[0])
+                delivery_list.append(file.split("_")[1])
+                cruise_list.append(file.split("_")[-1])
+                driver_list.append(driver_num)
+                matching_rate_list.append(get_matching_rate(records,order))
+                waiting_time_list.append(get_avg_prematching_waiting_time(records,order))
+                pickup_time_list.append(get_postmatching_pickup_time(records))
+                pickup_ratio_list.append(get_driver_pickup_ratio(records,start_time,end_time,driver_num))
+                usage_ratio_list.append(get_driver_usage_rate(records,start_time,end_time,driver_num))
 
 
 
+result_pd['driver_num'] = driver_list
+result_pd['pickup'] = pickup_list
+result_pd['delivery'] = delivery_list
+result_pd['cruise'] = cruise_list
+result_pd['matching rate'] = matching_rate_list
+result_pd['waiting time'] = waiting_time_list
+result_pd['pickup time'] = pickup_time_list
+result_pd['pickup ratio'] = pickup_ratio_list
+result_pd['usage ratio'] = usage_ratio_list
+
+result_pd.to_csv("analysis.csv")
