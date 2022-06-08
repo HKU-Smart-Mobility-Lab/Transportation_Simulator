5145edb815a9479143925fb208ecf5649bdd4ab9
chenhaoling <18395589135@163.com>
2022-06-08 09:22:10 +0800

    --ignore

--- simulator/test2.py
+++ simulator/test2.py
@@ -1,17 +1,32 @@
-# from utilities import *
-import sys
+# # from utilities import *
+# import sys
+#
+# import numpy as np
+# import pandas as pd
+#
+# import pickle
+#
+# records = pickle.load(open('dataset.pickle','rb'))
+#
+# for time in records.keys():
+#     for order in records[time]:
+#             print(order)
+#             sys.exit()
+#
+#
+# pickle.dump(records,open('dataset.pickle','wb'))
 
-import numpy as np
-import pandas as pd
+from azureml.opendatasets import NycTlcYellow
 
-import pickle
-
-records = pickle.load(open('dataset.pickle','rb'))
-
-for time in records.keys():
-    for order in records[time]:
-            print(order)
-            sys.exit()
+from datetime import datetime
+from dateutil import parser
 
 
-pickle.dump(records,open('dataset.pickle','wb'))
+end_date = parser.parse('2015-05-02')
+start_date = parser.parse('2015-05-01')
+nyc_tlc = NycTlcYellow(start_date=start_date, end_date=end_date)
+print(nyc_tlc)
+nyc_tlc_df = nyc_tlc.to_pandas_dataframe()
+
+nyc_tlc_df.info()
+print(nyc_tlc_df)
